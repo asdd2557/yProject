6d9fb84c4a73cab9e57f382bd736c88c1948fda6
asdd2557 <asdd2557@naver.com>
2024-04-02 19:56:02 +0900

    first commit

--- src/main/java/com/example/gameproject/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gameproject.repository;
-
-import com.example.gameproject.entity.Article_E;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article_E, Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article_e (title, content, author, created_at, updated_at) VALUES ('1', '1', 'user1', NOW(), NOW());
-INSERT INTO article_e (title, content, author, created_at, updated_at) VALUES ('2', '2', 'user2', NOW(), NOW());
-INSERT INTO article_e (title, content, author, created_at, updated_at) VALUES ('3', '3', 'user3', NOW(), NOW());
